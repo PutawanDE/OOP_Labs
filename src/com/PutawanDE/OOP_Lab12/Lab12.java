8c30dd2783b88f1483f1ddd242692863db2deae5
Tanadol Deachprapakorn <putawan-de@hotmail.co.th>
2021-12-23 00:48:23 +0700

    Finish lab12

--- src/com/PutawanDE/OOP_Lab12/Lab12.java
+++ src/com/PutawanDE/OOP_Lab12/Lab12.java
@@ -1,21 +1,18 @@
 package com.PutawanDE.OOP_Lab12;
 
 import java.io.*;
-import java.util.InputMismatchException;
-import java.util.Stack;
 
 public class Lab12 {
     public static void main(String[] args) {
-        String inFileName = "src/com/PutawanDE/OOP_Lab09/lab09_test3.txt";
-        String outFileName = "src/com/PutawanDE/OOP_Lab09/lab09_output.txt";
+        String inFileName = "src/com/PutawanDE/OOP_Lab12/lab12_test2.txt";
+        String outFileName = "src/com/PutawanDE/OOP_Lab12/lab12_output.txt";
 
-        for (int i = 0; i < 100000; i++) {
-            readAndCalculate(inFileName, outFileName);
-        }
+        readAndCalculate(inFileName, outFileName);
     }
 
     public static void readAndCalculate(String inFileName, String outFileName) {
         String inLine;
+        ExpEvaluatorLL1 expParser = new ExpEvaluatorLL1();
 
         try (FileReader fileReader = new FileReader(inFileName);
              BufferedReader br = new BufferedReader(fileReader);
@@ -29,98 +26,17 @@ public class Lab12 {
                 if (inLine.length() == 0) continue;
 
                 try {
-                    String outLine = inLine + " = " + interpretAndCalculate(inLine, lineNum) + "\n";
+                    double result = expParser.evaluateExp(inLine, lineNum);
+                    String outLine = inLine + " = " + result + "\n";
                     System.out.print(outLine);
                     bw.append(outLine);
-                } catch (ArithmeticException | InputMismatchException e) {
+                } catch (SyntaxError | ArithmeticException e) {
                     System.out.println("Invalid line: " + inLine);
                     e.printStackTrace();
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static double interpretAndCalculate(String line, int lineNum)
-            throws InputMismatchException, ArithmeticException {
-        Stack<Double> operandStack = new Stack<>();
-        Stack<Character> operatorStack = new Stack<>();
-        StringBuilder operandStr = new StringBuilder();
-        boolean expectingOperator = false;
-
-        for (int i = 0; i < line.length(); i++) {
-            char c = line.charAt(i);
-            boolean isEndOfLine = i == (line.length() - 1);
-            String charPos = "[Ln:" + lineNum + ", Col:" + (i + 1) + "]";
-
-            // Parse Number only
-            // If End of Line and is not number, then this line is invalid
-            if (isEndOfLine && !Character.isDigit(c)) {
-                throw new InputMismatchException("Invalid token: '" + c + "' at the end of line. " + charPos);
-            } else if (Character.isDigit(c)) {
-                if (!expectingOperator) operandStr.append(c);
-                else throw new InputMismatchException("Space between number digits is not allowed.  " + charPos);
-            }
-
-            // If an operator is found, or end of line is reached,
-            // then trigger the calculation of the last 2 operands
-            if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || isEndOfLine) {
-                expectingOperator = false;
-
-                if (operandStr.length() > 0) {
-                    String numStr = operandStr.toString().trim();
-                    double operand = Integer.parseInt(numStr);
-                    operandStack.push(operand);
-                } else {
-                    throw new InputMismatchException("Operator mismatch. " + charPos);
-                }
-
-                if (operandStack.size() == 2) {
-                    char operator = operatorStack.pop();
-                    double op2 = operandStack.pop();
-                    double op1 = operandStack.pop();
-                    double res = 0;
-
-                    switch (operator) {
-                        case '+':
-                            res = op1 + op2;
-                            break;
-                        case '-':
-                            res = op1 - op2;
-                            break;
-                        case '*':
-                            res = op1 * op2;
-                            break;
-                        case '/':
-                            res = op1 / op2;
-                            break;
-                        case '%':
-                            res = op1 % op2;
-                            break;
-                    }
-
-                    operandStack.push(res);
-                }
-                operatorStack.push(c);
-                operandStr.setLength(0); // Empty string builder
-            } else if (c == ' ') {
-                if (operandStr.length() > 0) expectingOperator = true;
-            } else if (!Character.isDigit(c)) { // Not number, space, operator, or end of line
-                throw new InputMismatchException("Invalid token: '" + c + "' at " + charPos);
-            }
-        }
-
-        double result = operandStack.pop();
-
-        // Check if result is valid (Not NaN, +-Infinity)
-        // From https://stackoverflow.com/questions/14137989/
-        // java-division-by-zero-doesnt-throw-an-arithmeticexception-why/14138002#comment119309143_14138032
-        if (!Double.isFinite(result))
-            throw new ArithmeticException(" NaN, or Infinity, or -Infinity " +
-                    "- may caused by division by zero. [Ln:" + lineNum + "]");
-
-        return result;
     }
 }
---
+++ src/com/PutawanDE/OOP_Lab12/Lab12Test.java
@@ -0,0 +1,57 @@
+package com.PutawanDE.OOP_Lab12;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Lab12Test {
+    ExpEvaluatorLL1 expParser = new ExpEvaluatorLL1();
+
+    @Test
+    void properLineFormatTest() throws SyntaxError {
+        assertEquals(62.0, expParser.evaluateExp("11+51", 1));
+        assertEquals(1.0, expParser.evaluateExp("10 % 3", 1));
+        assertEquals(262443.8995957429,
+                expParser.evaluateExp("151115*21211/12121- 2331 +333 + 0", 1));
+        assertEquals(640.0, expParser.evaluateExp("(26+54)*(138-130)", 1));
+        assertEquals(19.0, expParser.evaluateExp("(64 +31)/( 80-75)", 1));
+        assertEquals(1922.8653846153845, expParser.evaluateExp("((2322-121+21)*45-1)/52", 1));
+        assertEquals(3.0, expParser.evaluateExp("3121%5*12-(30/5+3)", 1));
+        assertEquals(5.0, expParser.evaluateExp("(30 * 2) / ((28 - 26) * (19 - (13)))", 1));
+        assertEquals(7.0, expParser.evaluateExp("  1+ 6", 1));
+    }
+
+    @Test
+    void improperLineFormatTest() {
+        assertThrows(ArithmeticException.class, () -> expParser.evaluateExp("6/(10-3*3-1)", 1));
+        assertThrows(ArithmeticException.class, () -> expParser.evaluateExp("31/(10%3 *2 -(6-4))", 1));
+        assertThrows(ArithmeticException.class, () -> expParser.evaluateExp("0/0", 1));
+        assertThrows(SyntaxError.class, () -> expParser.evaluateExp("3 + 7 69", 1));
+        assertThrows(SyntaxError.class, () -> expParser.evaluateExp("151 5 +", 1));
+        assertThrows(SyntaxError.class, () -> expParser.evaluateExp("511+", 1));
+        assertThrows(SyntaxError.class, () -> expParser.evaluateExp("x^2+4x+10", 1));
+        assertThrows(SyntaxError.class, () -> expParser.evaluateExp("()", 1));
+        assertThrows(SyntaxError.class, () -> expParser.evaluateExp("51--15", 1));
+        assertThrows(SyntaxError.class, () -> expParser.evaluateExp("/15", 1));
+    }
+
+    @Test
+    void readWriteFileTest() throws IOException {
+        Path testFile = Paths.get("src/com/PutawanDE/OOP_Lab12/lab12_test2.txt");
+        Path output = Paths.get("src/com/PutawanDE/OOP_Lab12/lab12_test2_out.txt");
+        Path expectedFile = Paths.get("src/com/PutawanDE/OOP_Lab12/lab12_test2_expected.txt");
+        Lab12.readAndCalculate(testFile.toString(), output.toString());
+        Charset charset = StandardCharsets.US_ASCII;
+        List<String> outFileLines = Files.readAllLines(output, charset);
+        List<String> expectedFileLines = Files.readAllLines(expectedFile, charset);
+        assertEquals(outFileLines, expectedFileLines);
+    }
+}
